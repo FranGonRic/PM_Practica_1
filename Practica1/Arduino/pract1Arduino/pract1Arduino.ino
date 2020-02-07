c4a881294defc5a3e6b516976f10924a873e9558
SantiagoBorobia <santiagoborobia1@hotmail.com>
2020-02-07 14:49:07 -0600

    Add existing file

---
+++ Practica1/Arduino/pract1Arduino/pract1Arduino.ino
@@ -0,0 +1,76 @@
+/*
+  Blink
+
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://www.arduino.cc/en/Main/Products
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  modified 8 Sep 2016
+  by Colby Newman
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/Blink
+*/
+/*
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);                       // wait for a second
+}
+*/
+
+/* Use a photoresistor (or photocell) to turn on an LED in the dark
+   More info and circuit schematic: http://www.ardumotive.com/how-to-use-a-photoresistor-en.html
+   Dev: Michalis Vasilakis // Date: 8/6/2015 // www.ardumotive.com */
+   
+
+//Constants
+const int pResistor = A0; // Photoresistor at Arduino analog pin A0
+const int ledPin=9;// Led pin at Arduino pin A9
+const int potenPin = A1; // Input from Potenciometro
+
+//Variables
+double value;          // Store value from photoresistor (0-1023)
+double threshold;
+
+void setup(){
+ pinMode(ledPin, OUTPUT);  // Set lepPin - 9 pin as an output
+ pinMode(pResistor, INPUT);// Set pResistor - A0 pin as an input (optional)
+ pinMode(potenPin, INPUT);
+ Serial.begin(9600);
+}
+
+void loop(){
+  value = analogRead(pResistor);
+  threshold = analogRead(potenPin);
+  //You can change value "100"
+  if (value > threshold){
+    digitalWrite(ledPin, LOW);  //Turn led off
+  }
+  else{
+    digitalWrite(ledPin, HIGH); //Turn led on
+  }
+   Serial.println("Potenciometro: ");
+   Serial.println(threshold/1023);
+   Serial.println("Sensor: ");
+   Serial.println(value/1023);
+  delay(500); //Small delay
+}
